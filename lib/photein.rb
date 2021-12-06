58c7992bc2302711f39b4829f221448c0ea90f9b
Ryan Lue <hello@ryanlue.com>
2021-12-06 10:17:03 -0800

    rfg: Move core logic into Photein.run method

--- lib/photein.rb
+++ lib/photein.rb
@@ -8,4 +8,22 @@ require 'photein/image'
 require 'photein/video'
 
 module Photein
+  class << self
+    def run
+      # Video compression is time-consuming, so save it for last
+      Pathname(Photein::Config.source)
+        .join(Photein::Config.recursive ? '**' : '')
+        .join("*{#{Photein::Image::SUPPORTED_FORMATS.join(',')}}")
+        .then { |glob| Dir.glob(glob, File::FNM_CASEFOLD).sort }
+        .map(&Photein::Image.method(:new))
+        .each(&:import)
+
+      Pathname(Photein::Config.source)
+        .join(Photein::Config.recursive ? '**' : '')
+        .join("*{#{Photein::Video::SUPPORTED_FORMATS.join(',')}}")
+        .then { |glob| Dir.glob(glob, File::FNM_CASEFOLD).sort }
+        .map(&Photein::Video.method(:new))
+        .each(&:import)
+    end
+  end
 end
